8c22169ba386f3a51db3bf69c2be46d4fb91eb85
Alex <alex.arenols@depruebas.com>
2021-02-14 11:26:16 +0100

    initial commit

---
+++ select1.php
@@ -0,0 +1,31 @@
+<?php
+
+	use Doctrine\ORM\Tools\Setup;
+	use Doctrine\ORM\EntityManager;
+
+	require_once "vendor/autoload.php";
+
+	$connectionParams = array(
+    'dbname' => 'pruebas',
+    'user' => 'root',
+    'password' => '',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+	);
+
+	$conn = \Doctrine\DBAL\DriverManager::getConnection( $connectionParams);
+
+	print_r ( $conn);
+
+	$sql = "SELECT * FROM areas";
+	$stmt = $conn->query( $sql);
+
+	/*$row = $stmt->fetchAllAssociative();
+
+	print_r ( $row);*/
+
+	while (($row = $stmt->fetchAssociative()) !== false) {
+	    echo $row['1'] . "\n";
+	}
+
+	//print_r ( $stmt);
